136bd886ff5093d163e28b0104621e09d0865ebb
James Taber <jmtaber129@gmail.com>
2020-01-16 11:05:17 -0500

    Updates

---
+++ precache-manifest.a1b0ff059f8c90384b4c3d950fd2d571.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90b22089e2be8fd83df87d833969eae0",
+    "url": "/adult-playfulness-scale/index.html"
+  },
+  {
+    "revision": "6f3426d915704e60a0ef",
+    "url": "/adult-playfulness-scale/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "c3ccfbb612232e1290a4",
+    "url": "/adult-playfulness-scale/static/js/2.c39a7159.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/adult-playfulness-scale/static/js/2.c39a7159.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6f3426d915704e60a0ef",
+    "url": "/adult-playfulness-scale/static/js/main.edbac14c.chunk.js"
+  },
+  {
+    "revision": "b40dd463db121957f8bf",
+    "url": "/adult-playfulness-scale/static/js/runtime-main.2db40570.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/adult-playfulness-scale/static/media/logo.5d5d9eef.svg"
+  }
+]);
